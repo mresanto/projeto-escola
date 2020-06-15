6f7f1c46520c29df2156b5542db6a4c13cf821a8
xNyku <micaelrodrigues504@gmail.com>
2020-06-15 15:51:13 -0300

    Subindo projeto

---
+++ MainActivity.java
@@ -0,0 +1,135 @@
+package com.exercicos.aulas.soma;
+/*
+bibliotecas que são necessários para o funcionamento do projeto, equivalente ao Using do C#.
+Quando é necessário importar uma biblioteca a classe fica em vermelho por não ser reconhecida. Para corrigir pode :
+1 - digitar o import manualmente
+2 - clicar sobre a classe com erro e aguardar a solicitação do Android Studio para pressionar ALt+Tab
+3 - clicar com o botão direito sobra a classe e selecionar a opção importar
+*/
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+/*
+    Cria uma instância dos elementos utilizados na tela
+    EditText --> Caixa de edição de texto
+    TextView --> Exibição de texto ( equivale ao Label do C#)
+ */
+
+    EditText num1;
+    EditText num2;
+    TextView result;
+    Button btnSoma;
+
+    //Variaveis locais para manipulação de Valores
+    int n1,n2,resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+    associar os elementos locais aos elementos de tela através da propriedade ID
+    onde:
+        findViewById --> método que localiza os elementos
+        R --> Classe autogerada que mapeia a View
+ */
+        num1 = (EditText)findViewById(R.id.num1);
+        num2 = (EditText)findViewById(R.id.num2);
+        result = (TextView)findViewById(R.id.resultado);
+        btnSoma = (Button)findViewById(R.id.btnSoma);
+		btnSubtracao = (Button)findViewById(R.id.btnSubtracao);
+		btnDivisao = (Button)findViewById(R.id.btnDivisao);
+		btnMulti = (Button)findViewById(R.id.btnMultiplicacao);
+
+    }
+    public void Soma(View view){
+		try{
+        n1 = Integer.parseInt(num1.getText().toString());
+        n2 = Integer.parseInt(num2.getText().toString());
+        resultado = n1 + n2;
+		 result.setText(Integer.toString(resultado));
+		}
+		catch (NumberFormatException)
+		{
+		 result.setText(Integer.toString("Texto digitado não é considerado número"));
+		}
+		catch (ArithmeticException)
+		{
+		 result.set.Text(Integer.toString("Por favor não utilize o número 0");
+		}
+		catch
+		{
+		 result.set.Text(Integer.toString("Algum erro que eu não sei");
+		}
+    }
+	    public void Subtracao(View view){
+		try{
+        //recupera o valor do elemento
+        n1 = Integer.parseInt(num1.getText().toString());
+        n2 = Integer.parseInt(num2.getText().toString());
+        resultado = n1 - n2;
+        result.setText(Integer.toString(resultado));
+		}
+		catch (NumberFormatException)
+		{
+		 result.setText(Integer.toString("Texto digitado não é considerado número"));
+		}
+		catch (ArithmeticException)
+		{
+		 result.set.Text(Integer.toString("Por favor não utilize o número 0");
+		}
+		catch
+		{
+		 result.set.Text(Integer.toString("Algum erro que eu não sei");
+		}
+    }
+	
+	    public void Divisao(View view){
+		try{
+        n1 = Integer.parseInt(num1.getText().toString());
+        n2 = Integer.parseInt(num2.getText().toString());
+        resultado = n1 / n2;
+        result.setText(Integer.toString(resultado));
+		}
+		catch (NumberFormatException)
+		{
+		 result.setText(Integer.toString("Texto digitado não é considerado número"));
+		}
+		catch (ArithmeticException)
+		{
+		 result.set.Text(Integer.toString("Por favor não utilize o número 0");
+		}
+		catch
+		{
+		 result.set.Text(Integer.toString("Algum erro que eu não sei");
+		}
+		
+    }
+	    public void Multiplicacao(View view){
+		try{
+        n1 = Integer.parseInt(num1.getText().toString());
+        n2 = Integer.parseInt(num2.getText().toString());
+        resultado = n1 * n2;
+        result.setText(Integer.toString(resultado));
+		}
+		catch (NumberFormatException)
+		{
+		 result.setText(Integer.toString("Texto digitado não é considerado número"));
+		}
+		catch (ArithmeticException)
+		{
+		 result.set.Text(Integer.toString("Por favor não utilize o número 0");
+		}
+		catch
+		{
+		 result.set.Text(Integer.toString("Algum erro que eu não sei");
+		}
+    }
+
+
+}
